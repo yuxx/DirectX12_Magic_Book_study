db8c4b3c6a6999b384016907d0648a3c9c2144f5
yu++ <7052856+yuxx@users.noreply.github.com>
2024-10-06 23:05:57 +0900

    add show empty window

--- DirectX12_test01/main.cpp
+++ DirectX12_test01/main.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <tchar.h>
 #ifdef _DEBUG
 #include <iostream>
 #endif // _DEBUG
@@ -19,6 +20,19 @@ void DebugOutputFormatString(const char* format, ...)
 #endif // _DEBUG
 }
 
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	if (msg == WM_DESTROY) {
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+const unsigned int window_width = 1280;
+const unsigned int window_height = 720;
+
 #ifdef _DEBUG
 int main()
 #else
@@ -26,7 +40,50 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 #endif // _DEBUG
 {
 	DebugOutputFormatString("Show window test.");
-	getchar();
+//	getchar();
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure;
+	w.lpszClassName = _T("DX12Sample");
+	w.hInstance = GetModuleHandle(nullptr);
+
+	RegisterClassEx(&w);
+
+	RECT wrect = { 0, 0, window_width, window_height };
+
+	AdjustWindowRect(&wrect, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hwnd = CreateWindow(
+		w.lpszClassName,
+		_T("DirectX12ƒeƒXƒg"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrect.right - wrect.left,
+		wrect.bottom - wrect.top,
+		nullptr,
+		nullptr,
+		w.hInstance,
+		nullptr
+	);
+
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg = {};
+
+	while (true) {
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		if (msg.message == WM_QUIT) {
+			break;
+		}
+	}
+
+	UnregisterClass(w.lpszClassName, w.hInstance);
 	
 	return 0;
 }
