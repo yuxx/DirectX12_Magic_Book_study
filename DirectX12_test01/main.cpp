93e766eb0e9cff5e25a98264f09e42f529d242c7
yu++ <7052856+yuxx@users.noreply.github.com>
2024-10-06 20:34:27 +0900

    change printf to vprintf, because it is more secure.

--- DirectX12_test01/main.cpp
+++ DirectX12_test01/main.cpp
@@ -14,7 +14,7 @@ void DebugOutputFormatString(const char* format, ...)
 #ifdef _DEBUG
 	va_list valist;
 	va_start(valist, format);
-	printf(format, valist);
+	vprintf(format, valist);
 	va_end(valist);
 #endif // _DEBUG
 }
